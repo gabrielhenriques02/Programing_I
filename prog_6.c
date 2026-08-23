f76ccca1a0c20747ad0a38801fe5d0bbee019d61
Gabriel Henriques <gabrielhenriques1603@gmail.com>
2026-08-23 12:03:45 -0300

    prog_6 quase pronto

--- prog_6.c
+++ prog_6.c
@@ -10,10 +10,15 @@ int main(){
     printf("Type C (integer): ");
     scanf("%d", &C);
 
-    if (A > B){
-        if(A > C){
-            printf("A (%d) is the largest number.");
-        }
+    if (A > B && A > C){
+        printf("A (%d) is the largest number.\n", A);
     }
+    else if (B > A && B > C){
+        printf("B (%d) is the largest number.\n", B);
+    }
+    else if (C > A && C > B) {
+        printf("C (%d) is the largest number.\n", C);
+    }
+    //TODO: loop to make shore that all number are diferent.
 
 }

1e76a759d24551a47e5e4287443ca0994644f220
Gabriel Henriques <gabrielhenriques1603@gmail.com>
2026-08-25 10:52:39 -0300

    .gitignore ON, remove executables and enviroment configurations

--- prog_7.c
+++ prog_7.c
@@ -6,12 +6,12 @@ int main() {
     int year;
 
     printf("type the year: ");
-    scanf("%d", &year);
+    scanf("%i", &year);
 
     if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-        printf("%d is a LEAP YEAR.\n", year);
+        printf("%i is a LEAP YEAR.\n", year);
     }
     else {
-        printf("%d is not a LEAP YEAR.\n", year);
+        printf("%i is not a LEAP YEAR.\n", year);
     }
 }
